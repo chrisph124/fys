f40eeb23cc7f7a6956d1fae9f545e32a32ad42ef
chrisph124 <chris.pham124@gmail.com>
2017-07-14 17:59:18 +0700

    Refactor nav and footer code

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,13 +1,13 @@
 <template>
     <footer class="footer">
         <div class="container is-fluid">
-            <div class="columns">
-                <div class="column text-left">
+            <div class="columns is-12">
+                <div class="column is-3 has-text-centered">
                     <p class="title">Headquarters</p>
                     <p class="heading">Techmaster Vietnam</p>
                     <p class="heading">Số 14 ngõ 4 Nguyễn Đình Chiểu, Hà Nội</p>
                 </div>
-                <div class="column text-left">
+                <div class="column is-3 has-text-centered">
                     <p class="title">Chính sách</p>
                     <p class="heading">
                         <a href="/privacy-policy">Chính sách bảo mật</a>
@@ -16,7 +16,7 @@
                         <a href="/security-policy">Quyền riêng tư</a>
                     </p>
                 </div>
-                <div class="column text-right">
+                <div class="column is-3 has-text-centered">
                     <p class="title">Hỗ trợ</p>
                     <p class="heading">
                         <a href="/faq">Câu hỏi thường gặp</a>
@@ -25,7 +25,7 @@
                         <a href="/terms-of-use">Điều khoản sử dụng</a>
                     </p>
                 </div>
-                <div class="column text-right">
+                <div class="column is-3 has-text-centered">
                     <p class="title">Chúng tôi</p>
                     <p class="heading">
                         <a href="/about">Giới thiệu FYS</a>
@@ -39,69 +39,71 @@
                 </div>
             </div>
             <hr/>
-            <div class="level">
-                <div class="level-left">
+                <div class="column is-4">
                     <p>
                         © 2017 - FYS. All Rights Reserved
                     </p>
                 </div>
-                <div class="level-left">
-                    <img src="~assets/img/fys-logo-symbol-white.png" />
+                <div class="column is-4">
+                    <img src="~assets/img/fys-logo-symbol.png" />
                 </div>
-                <div class="level-right">
-                    <a class="icon" href="https://github.com/jgthms/bulma">
+                <div class="column is-4">
+                    <a class="icon is-pulled-right" href="https://github.com/jgthms/bulma">
                         <i class="fa fa-github-square fa-3x" aria-hidden="true"></i>
                     </a>
-                    <a class="icon" href="https://github.com/jgthms/bulma">
+                    <a class="icon is-pulled-right" href="https://github.com/jgthms/bulma">
                         <i class="fa fa-facebook-square fa-4x" aria-hidden="true"></i>
                     </a>
                 </div>
-            </div>
         </div>
     </footer>
 </template>
 
 <style scoped>
 .footer {
-    background-color: #00d1b2;
-    padding: 0
+    background-color: #212121;
+    padding: 0;
+    margin-top: 2.5rem
 }
 
-.container {
-    padding: 30px 0
+p, a {
+    text-align: center;
+    font-weight: 500;
+    color: #FFF;
+    font-size: 0.9rem;
 }
 
-.level-left img {
+.is-4 img {
     width: 1.2rem
 }
 
 .column .title {
     font-size: 1.2rem;
     font-weight: bold;
+    color: #00d1b2;
+}
+
+
+
+.is-4 {
     color: white;
+    float: left;
 }
 
-.column .heading {
-    color: antiquewhite;
-    font-size: 0.8rem;
-}
-
-.column .heading a {
-    text-align: left;
-    color: antiquewhite;
-    font-size: 0.8rem;
-}
-
-.level {
-    color: white
-}
-
-.level .icon .fa {
+.is-4 .icon .fa {
     color: white;
     font-size: 1.8rem;
 }
 
-.level .icon:first-child {
-    margin-right: 15px
+.is-4 p {
+    text-align: left
+}
+
+.is-4 a {
+    text-align: right
+}
+
+.is-4 .icon:first-child {
+    margin-left: 1rem
 }
 </style>
--- components/Nav.vue
+++ components/Nav.vue
@@ -1,16 +1,14 @@
 <template>
   <div>
     <nav class="navbar">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/" id='logo'>
-          <img src="~assets/img/fys-logo.png">
-        </a>
-      </div>
   
       <div id="navMenuExample" class="navbar-menu">
         <div class="navbar-start">
+            <a class="navbar-item" href="/" id='logo'>
+              <img src="~assets/img/fys-logo.png">
+            </a>
           <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link  is-active" href="/documentation/overview/start/">
+            <a class="navbar-link" href="/documentation/overview/start/">
               Bán
             </a>
             <div class="navbar-dropdown ">
@@ -27,7 +25,7 @@
             </div>
           </div>
           <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link  is-active" href="/documentation/overview/start/">
+            <a class="navbar-link" href="/documentation/overview/start/">
               Cho thuê
             </a>
             <div class="navbar-dropdown ">
@@ -46,25 +44,25 @@
   
             </div>
           </div>
-          <a class="navbar-item" href="http://bulma.io/blog/">
+          <a class="navbar-item" href="/blog">
             Blog
           </a>
-          <a class="navbar-item " href="http://bulma.io/">
+          <a class="navbar-item " href="/faq">
             FAQ
           </a>
-          <a class="navbar-item " href="http://bulma.io/">
+          <a class="navbar-item " href="/contact">
             Liên Hệ
           </a>
   
           <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link  is-active" href="/documentation/overview/start/">
+            <a class="navbar-link" href="/documentation/overview/start/">
               Tài khoản của tôi
             </a>
             <div class="navbar-dropdown ">
-              <a class="navbar-item " href="/documentation/overview/start/">
+              <a class="navbar-item " href="/log-in">
                 Đăng nhập
               </a>
-              <a class="navbar-item " href="http://bulma.io/documentation/modifiers/syntax/">
+              <a class="navbar-item " href="/sign-up">
                 Đăng ký
               </a>
             </div>
@@ -76,8 +74,8 @@
           <div class="navbar-item">
             <div class="field is-grouped">
               <p class="control">
-                <a class="button is-primary">
-                  <span>Đăng tin mua - bán , cho thuê</span>
+                <a class="button is-primary" href="/posts">
+                  Đăng tin mua - bán , cho thuê
                 </a>
               </p>
             </div>
@@ -99,4 +97,40 @@
   max-height: 2.5rem;
 }
 
+.navbar,
+.navbar-dropdown,
+.navbar-item.has-dropdown:hover .navbar-link {
+  background-color: #212121;
+  border-top: none
+}
+
+.navbar {
+  height: 4rem;
+  margin-bottom: 2.5rem;
+}
+
+.navbar-item,
+.navbar-link {
+  color: #FFF;
+}
+
+a.navbar-item:hover {
+  color: #00d1b2;
+  background-color: transparent
+}
+
+.navbar-dropdown {
+  font-size: 1rem
+}
+
+a.navbar-item,
+a.navbar-link,
+a.navbar-link:hover {
+  background-color: transparent;
+  color: #FFF;
+}
+
+a.button {
+  font-size: 1.2rem
+}
 </style>
