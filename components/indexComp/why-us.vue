0ff2cc24fc49fbe03deb140ccfd88400380e8aa5
chrisph124 <chris.pham124@gmail.com>
2017-07-27 17:08:54 +0700

    Adding why-us block to index page

---
+++ components/indexComp/why-us.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3 class="title is-3 has-text-centered">Tại sao chọn FYS?</h3>
+    <div class="tile is-ancestor is-8">
+      <div class="tile is-vertical">
+        <div class="tile">
+          <div class="tile is-parent is-vertical">
+            <article class="tile is-child nofication is-primary">
+              <div class="card">
+                <div class="card-content">
+                  <p class="title">
+                    “There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”
+                  </p>
+                  <p class="subtitle">
+                    Jeff Atwood
+                  </p>
+                </div>
+              </div>
+            </article>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child">
+              
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
