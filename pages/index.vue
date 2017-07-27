5723a21e2ec262cb1328cc90c3d873a3b8df0b8a
Chris <chris.pham124@gmail.com>
2017-07-27 23:35:32 +0700

    75% completed

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,24 @@
 <template>
-    <div class="contaier-fluid is-marginless">
-        <div class="is-down">
-            <info-icon/>
-        </div>
-        <div class="is-down">
-        <real-estate/>
-        </div>
-        <div class="is-down">
-        <info-blog :getND="dataND" :getDA="dataDA" :getMV="dataMV"  />
-        </div>
-        <div class="is-down">
-        <why-us/>
-        </div>
+  <div class="contaier-fluid is-marginless">
+    <div class="grey">
+      <info-icon/>
     </div>
+    <div class="is-down">
+      <real-estate/>
+    </div>
+    <div class="is-down grey">
+      <info-blog :getND="dataND" :getDA="dataDA" :getMV="dataMV" />
+    </div>
+    <div class="is-down">
+      <find-by-building/>
+    </div>
+    <div class="is-down grey">
+      <find-by-district/>
+    </div>
+    <div class="is-down">
+      <why-us/>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -20,9 +26,11 @@ import axios from '~plugins/axios'
 import infoIcon from '~components/indexComp/info-icon-block.vue'
 import realEstate from '~components/indexComp/real-estate-block.vue'
 import infoBlog from '~components/indexComp/info-block.vue'
+import findByBuilding from '~components/indexComp/findByBuilding.vue'
+import findByDistrict from '~components/indexComp/findByDistrict.vue'
 import whyUs from '~components/indexComp/why-us.vue'
 export default {
-  components: { infoIcon, realEstate, infoBlog, whyUs },
+  components: { infoIcon, realEstate, infoBlog, findByBuilding, findByDistrict, whyUs },
   async asyncData () {
     let data1 = await axios.get('/api/blogfornd')
     let data2 = await axios.get('/api/blogforda')
@@ -37,4 +45,5 @@ export default {
 </script>
 
 <style scoped>
+
 </style>
