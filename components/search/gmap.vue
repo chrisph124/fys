d517c8949ec37119844b39a87926fd52298239fd
chrisph124 <chris.pham124@gmail.com>
2017-08-14 17:32:13 +0700

    Styling gmap

--- components/search/gmap.vue
+++ components/search/gmap.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <gmap-map :center="center" :zoom="15" class="column is-12" :options="{scrollwheel: mapScroll}">
-      <gmap-marker :key="index" v-for="(m, index) in markers" :position="m.position" :clickable="true" :draggable="false" @click="center=m.position"></gmap-marker>
+    <gmap-map :center='center' :zoom='15' class='column is-12' :options='{scrollwheel: mapScroll, styles: styles}'>
+      <gmap-marker :key='index' v-for='(m, index) in markers' :position='m.position' :clickable='true' :draggable='false' @click='center=m.position'></gmap-marker>
     </gmap-map>
   </div>
 </template>
@@ -27,7 +27,174 @@ export default {
         position: { lat: 21.028511, lng: 105.804817 }
       }, {
         position: { lat: 21.029132, lng: 105.806759 }
-      }]
+      }],
+      styles: [
+        {
+          'featureType': 'all',
+          'elementType': 'labels.text.fill',
+          'stylers': [
+            {
+              'saturation': 300
+            },
+            {
+              'color': '#00d1b2'
+            },
+            {
+              'lightness': 40
+            }
+          ]
+        },
+        {
+          'featureType': 'all',
+          'elementType': 'labels.text.stroke',
+          'stylers': [
+            {
+              'visibility': 'on'
+            },
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 16
+            }
+          ]
+        },
+        {
+          'featureType': 'all',
+          'elementType': 'labels.icon',
+          'stylers': [
+            {
+              'visibility': 'off'
+            }
+          ]
+        },
+        {
+          'featureType': 'administrative',
+          'elementType': 'geometry.fill',
+          'stylers': [
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 20
+            }
+          ]
+        },
+        {
+          'featureType': 'administrative',
+          'elementType': 'geometry.stroke',
+          'stylers': [
+            {
+              'color': '#00C853'
+            },
+            {
+              'lightness': 17
+            },
+            {
+              'weight': 1.2
+            }
+          ]
+        },
+        {
+          'featureType': 'landscape',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 20
+            }
+          ]
+        },
+        {
+          'featureType': 'poi',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 21
+            }
+          ]
+        },
+        {
+          'featureType': 'road.highway',
+          'elementType': 'geometry.fill',
+          'stylers': [
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 17
+            }
+          ]
+        },
+        {
+          'featureType': 'road.highway',
+          'elementType': 'geometry.stroke',
+          'stylers': [
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 29
+            },
+            {
+              'weight': 0.2
+            }
+          ]
+        },
+        {
+          'featureType': 'road.arterial',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 18
+            }
+          ]
+        },
+        {
+          'featureType': 'road.local',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 16
+            }
+          ]
+        },
+        {
+          'featureType': 'transit',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 19
+            }
+          ]
+        },
+        {
+          'featureType': 'water',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#90CAF9'
+            },
+            {
+              'lightness': 17
+            }
+          ]
+        }
+      ]
     }
   }
 }
@@ -37,6 +204,6 @@ export default {
 div {
   max-height: 100%;
   height: 850px;
-  margin: 0 0 2.5rem 0 
+  margin: 0 0 2.5rem 0
 }
 </style>
